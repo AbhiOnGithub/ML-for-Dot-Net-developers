9badd74a5b8722b09ef3c73e32bc11eab4575fa1
Abhishek <innovative.abhishek@hotmail.com>
2018-07-15 20:15:58 +0530

    Adding TestIrisData.cs

---
+++ 2-ClusteringInMLDotNET/ClusteringInML/ClusteringInML/IrisData.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.ML.Runtime.Api;
+
+namespace ClusteringInML
+{
+    public class IrisData
+    {
+        [Column("0")]
+        public float SepalLength;
+
+        [Column("1")]
+        public float SepalWidth;
+
+        [Column("2")]
+        public float PetalLength;
+
+        [Column("3")]
+        public float PetalWidth;
+    }
+
+    public class ClusterPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public uint PredictedClusterId;
+
+        [ColumnName("Score")]
+        public float[] Distances;
+    }
+}
